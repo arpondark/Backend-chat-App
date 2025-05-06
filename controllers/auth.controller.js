d752e93fe94aaf7362a79ab12733308d73570b80
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-05-06 23:17:52 +0600

    auth added

---
+++ controllers/auth.controller.js
@@ -0,0 +1,96 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(401).json({
+        message: "Please fill in all fields",
+      });
+    }
+    if (!email.includes("@")) {
+      return res.status(401).json({
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 6) {
+      return res.status(401).json({
+        message: "Password must be at least six characters long",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(401).json({
+        message: "Email already exists",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    if (newUser) {
+      const token = generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        token: token, // Use the generated token
+      });
+    } else {
+      res.status(400).json({
+        message: "Invalid Data",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+
+  //res.send("signup routes");
+};
+
+// Login route
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try{
+    const user = await User.findOne({ email });
+    if(!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+
+     const isPasswordValid = await bcrypt.compare(password, user.password)
+
+     if(!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+     }
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+      //token: token, // Use the generated token
+    }
+
+
+  }catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Logout route
+export const logout = (req, res) => {
+  res.send("logout routes");
+};
